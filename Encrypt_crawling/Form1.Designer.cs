d351588dee34cf4067a9f3fd0be6349258d875ea
myeongsu0923 <dieseltg1@daegu.ac.kr>
2023-01-20 09:17:50 +0900

    Update source

---
+++ Encrypt_crawling/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Encrypt_crawling
+{
+    partial class EncryptForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EncryptForm));
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.lbKeyword = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.lbPassword = new MetroFramework.Controls.MetroLabel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.cbVisible = new MetroFramework.Controls.MetroCheckBox();
+            this.lbCount = new MetroFramework.Controls.MetroLabel();
+            this.tbCount = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.metroPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lbStatus.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStatus.Location = new System.Drawing.Point(119, 367);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(298, 34);
+            this.lbStatus.TabIndex = 0;
+            this.lbStatus.Text = "Status";
+            this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbKeyword
+            // 
+            this.lbKeyword.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lbKeyword.Location = new System.Drawing.Point(38, 304);
+            this.lbKeyword.Name = "lbKeyword";
+            this.lbKeyword.Size = new System.Drawing.Size(75, 32);
+            this.lbKeyword.TabIndex = 4;
+            this.lbKeyword.Text = "Search";
+            this.lbKeyword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbKeyword.Click += new System.EventHandler(this.lbKeyword_Click);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.metroPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("metroPanel1.BackgroundImage")));
+            this.metroPanel1.Controls.Add(this.label1);
+            this.metroPanel1.Controls.Add(this.pictureBox1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(-2, 28);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(550, 243);
+            this.metroPanel1.TabIndex = 5;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            this.metroPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.metroPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 16F);
+            this.label1.Location = new System.Drawing.Point(154, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ILOCK";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(177, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 202);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.tbSearch.Font = new System.Drawing.Font("나눔스퀘어_ac", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbSearch.Location = new System.Drawing.Point(119, 311);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(111, 25);
+            this.tbSearch.TabIndex = 6;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lbPassword.Location = new System.Drawing.Point(38, 458);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(90, 32);
+            this.lbPassword.TabIndex = 7;
+            this.lbPassword.Text = "Password";
+            this.lbPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.Font = new System.Drawing.Font("나눔스퀘어_ac Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSearch.Location = new System.Drawing.Point(433, 308);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(101, 93);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnEncrypt.FlatAppearance.BorderSize = 0;
+            this.btnEncrypt.Font = new System.Drawing.Font("나눔스퀘어_ac Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnEncrypt.Location = new System.Drawing.Point(38, 504);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(218, 33);
+            this.btnEncrypt.TabIndex = 9;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.tbPassword.Font = new System.Drawing.Font("나눔스퀘어_ac", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbPassword.Location = new System.Drawing.Point(119, 465);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(405, 25);
+            this.tbPassword.TabIndex = 10;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnDecrypt.FlatAppearance.BorderSize = 0;
+            this.btnDecrypt.Font = new System.Drawing.Font("나눔스퀘어_ac Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDecrypt.Location = new System.Drawing.Point(306, 504);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(218, 33);
+            this.btnDecrypt.TabIndex = 11;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // cbVisible
+            // 
+            this.cbVisible.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
+            this.cbVisible.Location = new System.Drawing.Point(38, 367);
+            this.cbVisible.Name = "cbVisible";
+            this.cbVisible.Size = new System.Drawing.Size(90, 39);
+            this.cbVisible.TabIndex = 13;
+            this.cbVisible.Text = "Unvisible";
+            this.cbVisible.UseSelectable = true;
+            // 
+            // lbCount
+            // 
+            this.lbCount.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lbCount.Location = new System.Drawing.Point(236, 304);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(75, 32);
+            this.lbCount.TabIndex = 14;
+            this.lbCount.Text = "Count";
+            this.lbCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbCount
+            // 
+            this.tbCount.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.tbCount.Font = new System.Drawing.Font("나눔스퀘어_ac", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbCount.Location = new System.Drawing.Point(306, 311);
+            this.tbCount.Name = "tbCount";
+            this.tbCount.Size = new System.Drawing.Size(111, 25);
+            this.tbCount.TabIndex = 15;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // EncryptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 584);
+            this.Controls.Add(this.tbCount);
+            this.Controls.Add(this.lbCount);
+            this.Controls.Add(this.cbVisible);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.lbKeyword);
+            this.Controls.Add(this.lbStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EncryptForm";
+            this.Text = "Encryption File";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Load += new System.EventHandler(this.EncryptForm_Load);
+            this.metroPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbStatus;
+        private MetroFramework.Controls.MetroLabel lbKeyword;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbSearch;
+        private MetroFramework.Controls.MetroLabel lbPassword;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnDecrypt;
+        private MetroFramework.Controls.MetroCheckBox cbVisible;
+        private MetroFramework.Controls.MetroLabel lbCount;
+        private System.Windows.Forms.TextBox tbCount;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Encrypt_crawling/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Encrypt_crawling
+{
+    public partial class EncryptForm : MetroFramework.Forms.MetroForm
+    {
+        protected ChromeDriverService _driverService = null;
+        protected ChromeOptions _options = null;
+        protected ChromeDriver _driver = null;
+
+        protected byte[] SALT = new byte[] { 0x43, 0x87, 0x23, 0x72 };
+
+        public EncryptForm()
+        {
+            InitializeComponent();
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("disable-gpu");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string drivePath = "C:\\Download\\Image\\";
+            string directoryPath = @"C:\Download\Image";
+            string zipPath = directoryPath + ".zip";
+
+            DirectoryInfo directory = new DirectoryInfo(drivePath);
+
+            if (directory.Exists == false)
+                directory.Create();
+
+            if (cbVisible.Checked == true)
+                _options.AddArgument("--headless");
+
+            int count = Convert.ToInt32(tbCount.Text);
+            int current = 0;
+            string search = tbSearch.Text;
+
+            IWebDriver _driver = new ChromeDriver(_driverService, _options);
+
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Url = "https://www.google.com/imghp";
+
+            var element = _driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+            element.Click();
+            element.SendKeys(search);
+            element.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+            IList<IWebElement> all = _driver.FindElements(By.CssSelector("img.rg_i.Q4LuWd"));
+            foreach (IWebElement elements in all)
+            {
+                string driveFile = drivePath + "Image" + current + ".jpg";
+                elements.Click();
+                Thread.Sleep(500);
+                Screenshot _screenshot = ((ITakesScreenshot)elements).GetScreenshot();
+                _screenshot.SaveAsFile(driveFile, ScreenshotImageFormat.Jpeg);
+                current++;
+                lbStatus.Text = current.ToString() + " Image Download";
+
+                if (current >= count)
+                {
+                    MessageBox.Show("Crawling OK");
+
+                    bool zipResult = ZipDirectory(directoryPath, zipPath);
+                    
+
+                    if (zipResult == true) lbStatus.Text += " , Zip OK";
+                    else lbStatus.Text += " , Zip Fail";
+                    directory.Delete(true);
+                    _driver.Close();
+
+                    break;
+                }
+            }
+        }
+
+        public byte[] Encrypt(byte[] input, string password, byte[] salt)
+        {
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(password, salt);
+            using (MemoryStream ms = new MemoryStream())
+            using (Aes aes = new AesManaged())
+            {
+                aes.Key = pdb.GetBytes(aes.KeySize / 8);
+                aes.IV = pdb.GetBytes(aes.BlockSize / 8);
+                CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                cs.Write(input, 0, input.Length);
+                cs.Close();
+                return ms.ToArray();
+            }
+        }
+        public byte[] Decrypt(byte[] input, string password, byte[] salt)
+        {
+            PasswordDeriveBytes pdb = new PasswordDeriveBytes(password, salt);
+            using (MemoryStream ms = new MemoryStream())
+            using (Aes aes = new AesManaged())
+            {
+
+                aes.Key = pdb.GetBytes(aes.KeySize / 8);
+                aes.IV = pdb.GetBytes(aes.BlockSize / 8);
+                CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write);
+                cs.Write(input, 0, input.Length);
+                cs.Close();
+                return ms.ToArray();
+
+            }
+        }
+
+        public static bool ZipDirectory(string directoryPath, string outputZipPath)
+        {
+            try
+            {
+                if (File.Exists(outputZipPath))
+                {
+                    File.Delete(outputZipPath);
+                }
+
+                ZipFile.CreateFromDirectory(directoryPath, outputZipPath);
+
+                return true;
+            }
+
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "암호화하려는 파일을 선택하세요";
+                ofd.InitialDirectory = @"C:\Download\";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using(SaveFileDialog sfd = new SaveFileDialog())
+                    {
+                        sfd.Title = "암호화된 파일을 저장할 경로를 입력하세요.";
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+
+                            try
+                            {
+                                File.WriteAllBytes(sfd.FileName
+                                    , Encrypt(File.ReadAllBytes(ofd.FileName)
+                                    , this.tbPassword.Text
+                                    , this.SALT));
+
+                                MessageBox.Show("저장되었습니다"
+                                    , "성공"
+                                    , MessageBoxButtons.OK
+                                    , MessageBoxIcon.Information);
+
+                                File.Delete(ofd.FileName);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message
+                                    , "오류"
+                                    , MessageBoxButtons.OK
+                                    , MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "복호화하려는 파일을 선택하세요";
+                ofd.InitialDirectory = @"C:\Download\";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (SaveFileDialog sfd = new SaveFileDialog())
+                    {
+                        sfd.Title = "복호화된 파일을 저장할 경로를 입력하세요";
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+
+                            try
+                            {
+
+                                File.WriteAllBytes(sfd.FileName
+                                    , Decrypt(File.ReadAllBytes(ofd.FileName)
+                                    , this.tbPassword.Text
+                                    , this.SALT));
+
+                                MessageBox.Show("저장되었습니다"
+                                    , "성공"
+                                    , MessageBoxButtons.OK
+                                    , MessageBoxIcon.Information);
+
+                                File.Delete(ofd.FileName);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message
+                                    , "오류"
+                                    , MessageBoxButtons.OK
+                                    , MessageBoxIcon.Error);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void metroPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void EncryptForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbKeyword_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
